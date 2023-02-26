f3fc78f1b34e480e3fdca624f354299b15dc58ed
afzal-khan-in <khanafzal196.com>
2023-02-26 21:48:21 +0530

    jsx

--- Chapter_03_Laying_the_foundation/App.js
+++ Chapter_03_Laying_the_foundation/App.js
@@ -1,28 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    className: "title",
-    key: "h1",
-  },
-  "Hello React World!!!"
+const heading1 = (
+  <h1 className="title" key="h1">
+    Hello React World
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h2",
-  {
-    className: "sub-title",
-    key: "h2",
-  },
-  "Hello to the world!!!"
+const heading2 = (
+  <h2 className="sub-title" key="h2">
+    Hello to the world
+  </h2>
 );
 
-const container = React.createElement("div", { id: "container" }, [
-  heading1,
-  heading2,
-]);
+// const container = React.createElement("div", { id: "container" }, [
+//   heading1,
+//   heading2,
+// ]);
+
+const container = (
+  <div className="container">
+    {heading1}
+    {heading2}
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
